aaf735087f0d183075fc38554d116ddca0cf6367
khadija98ehtp <khadijaasabre@gmail.com>
2020-10-28 13:31:01 +0000

    Your commit message

---
+++ src/main/java/com/flyBird/TransImmo/TransImmoApplication.java
@@ -0,0 +1,13 @@
+package com.flyBird.TransImmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransImmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransImmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyBird/TransImmo/TransImmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyBird.TransImmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransImmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
